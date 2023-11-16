0a20b1856a3f127db597263c0cd4c8a74abd84a1
Luiz Felipe <101591932+lufecrx@users.noreply.github.com>
2023-11-16 17:33:03 -0300

    Apagar o diretório ExerciciosDaAula/src da branch PO-002

--- ExerciciosDaAula/src/zoologico/Papagaio.java
+++
@@ -1,9 +0,0 @@
-package zoologico;
-
-public class Papagaio extends Animal {
-	
-	public Papagaio(String nome, String classe, double peso) {
-		super(nome, classe, peso);
-	}
-	
-}
--- ExerciciosDaAula/src/zoologico/Tigre.java
+++
@@ -1,9 +0,0 @@
-package zoologico;
-
-public class Tigre extends Animal {
-	
-	public Tigre(String nome, String classe, double peso) {
-		super(nome, classe, peso);
-	}
-
-}
