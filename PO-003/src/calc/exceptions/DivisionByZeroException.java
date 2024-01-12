d0f918f2b5456a785580644a0215b2a30b1dd5f6
lufecrx <luizfelipecrx@gmail.com>
2024-01-12 14:51:33 -0300

    Remover arquivos desnecessários para a branch

--- PO-003/src/calc/exceptions/DivisionByZeroException.java
+++
@@ -1,9 +0,0 @@
-package calc.exceptions;
-
-@SuppressWarnings("serial")
-public class DivisionByZeroException extends Exception {
-    
-	public DivisionByZeroException(String mensagem) {
-        super(mensagem);
-    }
-}
