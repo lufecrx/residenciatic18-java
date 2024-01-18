a431a01d323c1f7e73a2842bee7cdda38a3a6cac
lufecrx <luizfelipecrx@gmail.com>
2024-01-18 14:15:28 -0300

    Fazer alterações para a PO-007

---
+++ PO-007/FilesExercises/src/filemanager/JWriter.java
@@ -0,0 +1,11 @@
+package filemanager;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class JWriter {
+
+}
