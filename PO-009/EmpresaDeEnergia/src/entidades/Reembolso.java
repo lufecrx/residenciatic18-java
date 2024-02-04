02f9114c31dd6b0e7eff27cb86585191801ea0ac
lufecrx <luizfelipecrx@gmail.com>
2024-02-04 15:54:47 -0300

    Sincronizar reembolsos com o BD

--- PO-009/EmpresaDeEnergia/src/entidades/Reembolso.java
+++ PO-009/EmpresaDeEnergia/src/entidades/Reembolso.java
@@ -1,10 +1,13 @@
 package entidades;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Reembolso {
     private String idFatura;
-    private double valor;
+    private Double valor;
     private Calendar data;
 
     public Reembolso(String idFatura, double valor, Calendar data) {
@@ -17,11 +20,26 @@ public class Reembolso {
         return idFatura;
     }
 
-    public double getValor() {
+    public Double getValor() {
         return valor;
     }
 
     public Calendar getData() {
         return data;
+    }
+
+    public String dataParaString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return sdf.format(data.getTime());
+	}
+
+	public static Calendar stringParaCalendar(String dateString) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(dateString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return calendar;
     }
 }
