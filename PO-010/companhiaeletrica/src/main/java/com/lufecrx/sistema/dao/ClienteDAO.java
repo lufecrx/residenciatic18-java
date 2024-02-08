226438ef8ca9bb5ef7ebb3b990a4df01c7463373
lufecrx <luizfelipecrx@gmail.com>
2024-02-07 21:23:31 -0300

    Atualizar DAO de clientes

--- PO-010/companhiaeletrica/src/main/java/com/lufecrx/sistema/dao/ClienteDAO.java
+++ PO-010/companhiaeletrica/src/main/java/com/lufecrx/sistema/dao/ClienteDAO.java
@@ -1,109 +1,61 @@
 package com.lufecrx.sistema.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import com.lufecrx.sistema.entidades.Cliente;
-import com.lufecrx.sistema.entidades.Imovel;
 import com.lufecrx.sistema.servicos.ImovelService;
-import com.lufecrx.sistema.util.ImovelNaoEncontradoException;
 
 public class ClienteDAO {
 
-    public static boolean criar(Cliente cliente) {
-        String query = "INSERT INTO Cliente (nome, propriedade, cpf) VALUES (?, ?, ?)";
-        try (Connection connection = DataAcessObject.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, cliente.getNome());
-            statement.setString(2, cliente.getPropriedade().getMatricula());
-            statement.setString(3, cliente.getCpf());
-            statement.executeUpdate();
-            
-            return true;
-        } catch (SQLException e) {
-            return false;
-        }
+    public static void criar(Cliente cliente, EntityManager entityManager) {
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
     }
 
-    public static List<Cliente> retornarTodos(ImovelService imovelService) {
-        String query = "SELECT nome, propriedade, cpf FROM Cliente";
-        List<Cliente> clientes = new ArrayList<>();
+    public static List<Cliente> retornarTodos(ImovelService imovelService, EntityManager entityManager) {
+        String jpql = "Select c FROM Cliente c";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        List<Cliente> clientes = query.getResultList();
+
+        return clientes;
+    }
+
+    public static Cliente retornarPeloCPF(String cpf, ImovelService imovelService, EntityManager entityManager) {
+        String jpql = "Select c FROM Cliente c WHERE c.cpf = :cpf";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("cpf", cpf);
+
+        return query.getSingleResult();
+    }
+
+    public static void atualizar(Cliente cliente, EntityManager entityManager) {
+        String jpql = "SELECT c FROM Cliente c WHERE c.cpf = :cpf";
+
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("cpf", cliente.getCpf());
+
+        Cliente clienteAtualizado = query.getSingleResult();
+        clienteAtualizado.setNome(cliente.getNome());
+        clienteAtualizado.setPropriedade(cliente.getPropriedade());
+        entityManager.persist(clienteAtualizado);
+        entityManager.getTransaction().commit();
+    }
+
+    public static void deletar(String cpf, EntityManager entityManager) {
+        String jpql = "SELECT c FROM Cliente c WHERE c.cpf = :cpf";
         
-        try (Connection connection = DataAcessObject.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("cpf", cpf);
 
-            
-            while (resultSet.next()) {
-                String nome = resultSet.getString("nome");
-                String matriculaDoImovel = resultSet.getString("propriedade");
-                String cpf = resultSet.getString("cpf");
-
-                Imovel propriedade = imovelService.retornarPelaMatricula(matriculaDoImovel);
-
-                Cliente cliente = new Cliente(nome, cpf, propriedade);
-                clientes.add(cliente);        
-            }
-            return clientes;
-        } catch (SQLException e) {
-            return null;
-        } catch (ImovelNaoEncontradoException e) {
-            return null;
-        }
-    }
-
-    public static Cliente retornarPeloCPF(String cpf, ImovelService imovelService) {
-        String query = "SELECT cpf, nome, propriedade FROM Cliente WHERE cpf = ?";
-        try (Connection connection = DataAcessObject.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, cpf);
-            ResultSet resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                String nome = resultSet.getString("nome");
-                String matriculaDoImovel = resultSet.getString("propriedade");
-
-                Imovel propriedade = imovelService.retornarPelaMatricula(matriculaDoImovel);
-                return new Cliente(nome, cpf, propriedade);
-            }
-            return null;
-        } catch (SQLException e) {
-            return null;
-        } catch (ImovelNaoEncontradoException e) {
-            return null;
-        }
-    }
-
-    public static boolean atualizar(Cliente cliente) {
-        String query = "UPDATE Cliente SET nome = ?, propriedade = ? WHERE cpf = ?";
-        try (Connection connection = DataAcessObject.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, cliente.getNome());
-            statement.setString(2, cliente.getPropriedade().getMatricula());
-            statement.setString(3, cliente.getCpf());
-            statement.executeUpdate();
-
-            return true;
-        } catch (SQLException e) {
-            return false;
-        }
-    }
-
-    public static boolean deletar(String cpf) {
-        String query = "DELETE FROM Cliente WHERE cpf = ?";
-
-        try (Connection connection = DataAcessObject.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, cpf);
-            statement.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            return false;
-        }
+        Cliente cliente = query.getSingleResult();
+        entityManager.getTransaction().begin();
+        entityManager.remove(cliente);
+        entityManager.getTransaction().commit();
     }
 
 }
